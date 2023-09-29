516db496104c4137a543bf4332062af37c542cc4
pgyanesh <gyaneshp.dev@gmail.com>
2023-09-29 20:31:22 +0530

    Made changes to the dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+/*import About from './components/About';*/
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
 function App() {
+  const[mode,setMode]=useState('light')
+  const toggleMode=()=>{
+   if(mode==='light'){
+    setMode('dark')
+    document.body.style.backgroundColor='black'
+   }else{
+    setMode('light')
+    document.body.style.backgroundColor='white'
+   }
+  }
   return (
    <>
-   <Navbar/>
+   <Navbar mode={mode} toggleMode={toggleMode}/>
    <div className='container my-3'>
-    <About/>
-   <Textform heading = "Enter your text here"/>
+    {/*<About/>*/}
+   <Textform heading = "Enter your text here" mode={mode}/>
    </div>
    
    </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -29,11 +29,11 @@ export default function Textform(props) {
     const[text,setText] = useState("")
   return (
     <>
-    <div className='container'>
+    <div className='container'  style={{color:props.mode==='light'?'black':'white'}}>
         <h1>{props.heading}</h1>
 <div className="mb-3">
  
-<textarea className="form-control" value ={text} onChange={handleOnChange} id="myBox" rows="5"></textarea>
+<textarea className="form-control" value ={text} style={{backgroundColor:props.mode==='light'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="5"></textarea>
 <button className='btn btn-dark my-3 mx-1' onClick={clearText}>Clear Text</button>
 <button className='btn btn-dark my-3 mx-1' onClick={copyText}>Copy text</button>
 <button className='btn btn-dark my-3 mx-1' onClick={removeSpaces}>Remove Extra Spaces</button>
@@ -41,7 +41,7 @@ export default function Textform(props) {
 <button className='btn btn-dark my-3 mx-1' onClick={toLoCase}>To Lower Case</button>
 </div>
     </div>
-    <div className='container'>
+    <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
     <h2>Your Text Summary</h2>
     <p>Your text has {text.split(" ").length} words and {text.length} characters</p>
    </div>
