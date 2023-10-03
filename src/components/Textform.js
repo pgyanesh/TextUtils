a03034aee6c15033bde8588f7ef45d268cb28676
Gyanesh kumar padhiary <gyaneshp.dev@gmail.com>
2023-10-03 14:26:35 +0530

    Added some classes to the layout and functionalities

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -39,16 +39,16 @@ export default function Textform(props) {
 <div className="mb-3">
  
 <textarea className="form-control" value ={text} style={{backgroundColor:props.mode==='light'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="5"></textarea>
-<button className='btn btn-dark my-3 mx-1' onClick={clearText}>Clear Text</button>
-<button className='btn btn-dark my-3 mx-1' onClick={copyText}>Copy text</button>
-<button className='btn btn-dark my-3 mx-1' onClick={removeSpaces}>Remove Extra Spaces</button>
-<button className='btn btn-dark my-3 mx-1' onClick={toUpCase}>To Upper Case</button>
-<button className='btn btn-dark my-3 mx-1' onClick={toLoCase}>To Lower Case</button>
+<button className='btn btn-dark my-3 mx-1 my-1' onClick={clearText}>Clear Text</button>
+<button className='btn btn-dark my-3 mx-1  my-1' onClick={copyText}>Copy text</button>
+<button className='btn btn-dark my-3 mx-1  my-1' onClick={removeSpaces}>Remove Extra Spaces</button>
+<button className='btn btn-dark my-3 mx-1  my-1' onClick={toUpCase}>To Upper Case</button>
+<button className='btn btn-dark my-3 mx-1  my-1' onClick={toLoCase}>To Lower Case</button>
 </div>
     </div>
     <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
     <h2>Your Text Summary</h2>
-    <p>Your text has {text.split(" ").length} words and {text.length} characters</p>
+    <p>Your text has {text.split(" ").filter((ele)=>{return ele.length !=0}).length} words and {text.length} characters</p>
    </div>
    </>
   )
